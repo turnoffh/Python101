7742e474524776f77f7390ed0febebcb8607f828
David Morales <david4.morales8@gmail.com>
2025-04-07 22:12:29 +0000

    Juego piedra papel o tijera

--- main.py
+++ main.py
@@ -1,35 +1,70 @@
-import random
+import random 
+
 options = ('piedra', 'papel', 'tijera')
+rounds = 1
+computer_wins = 0 
+user_wins = 0
 
-user_option = input("piedra, papel o tijera => ")
+print("""
+      [  Bienvenido al juego Piedra, Papel o tijera ]
+                  >>> Ingresa una opción <<<
+      """)
 
-if not user_option in options:
-  print("esa opción no es válida")
-  exit()
+while True:
+    print('***' * 10)
+    print('Round ', rounds)
+    print('***' * 10)
 
-computer_option = random.choice(options)
-if user_option == computer_option:
-  print("Empate!")
-elif user_option == "piedra":
-  if computer_option == "tijera":
-    print("piedra gana a tijera")
-    print("user gano!")
-  else:
-    print("papel gana a piedra")
-    print("computer gano!")
-elif user_option == "papel":
-  if computer_option == "piedra":
-    print("papel gana a piedra")
-    print("user gano!")
-  else:
-    print("tijera gana a papel")
-    print("computer gano!")
-elif user_option == "tijera":
-  if computer_option == "papel":
-    print("tijera gana a papel")
-    print("user gano!")
-  else:
-    print("piedra gana a tijera")
-    print("computer gano!")
-else:
-  print("Opción no válida")
+    print(f'''
+🤖 Computer wins: {computer_wins}
+🙋 User wins: {user_wins}
+          ''')
+
+    user_option = input('>>> Piedra, papel o tijera => ').lower()
+    rounds += 1
+
+    if not user_option in options:
+        print('Esa opción no es valida')
+
+    computer_option = random.choice(options)
+
+    print('User option => ', user_option)
+    print('Computer option => ', computer_option)
+
+    if user_option == computer_option:
+        print('Empate!\n')
+    elif user_option == 'piedra':
+        if computer_option == 'tijera':
+            print('🪨 Piedra gana a tijera ✂️')
+            print('¡User gana!\n')
+            user_wins += 1
+        else:
+            print('📄 Papel gana a piedra 🪨')
+            print('¡Computer gana!\n')
+            computer_wins += 1
+    elif user_option == 'papel':
+        if computer_option == 'piedra':
+            print('📄 Papel gana a piedra 🪨')
+            print('¡User gana!\n')
+            user_wins += 1
+        else:
+            print('✂️ Tijera gana a papel 📄')
+            print('¡Computer gana!\n')
+            computer_wins += 1
+    elif user_option == 'tijera':
+        if computer_option == 'papel':
+            print('✂️ Tijera gana a papel 📄')
+            print('¡User gana!\n')
+            user_wins += 1
+        else:
+            print('🪨 Piedra gana a tijera ✂️')
+            print('¡Computer gana!\n')
+            computer_wins += 1
+
+    if computer_wins == 3:
+        print('🎖️ El ganador es Computer 🎖️')
+        break
+
+    if user_wins == 3:
+        print('🎖️ El ganador es User 🎖️')
+        break
