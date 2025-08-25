960090856f31318ebde7b07ce21a182184d915ab
David Morales <david4.morales8@gmail.com>
2025-08-25 23:35:46 +0000

    Mas de modulos, llamando al modulo desde otro archivo

---
+++ 102/app/main.py
@@ -0,0 +1,8 @@
+import mod
+
+key, values = mod.getPopulation()
+print(key, values)
+print(key)
+print(values)
+
+print(mod.A)
---
+++ 102/app/mod.py
@@ -0,0 +1,7 @@
+def getPopulation():
+    keys = ['col', 'bol']
+    values = [300, 400]
+    return keys, values
+
+
+A = 'Hola soy la variable A'
