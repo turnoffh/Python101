fe0d2fce5f8f4cda4ea3eeb77fbb9e55d7fc13eb
David Morales <david4.morales8@gmail.com>
2025-08-26 23:54:44 +0000

    Manejo de varias excepiones con try

---
+++ S102/22_errorstry.py
@@ -0,0 +1,21 @@
+try:
+  print(0/0)
+  assert 1 != 1, 'Uno no es igual que uno'
+  age = 10
+  if age < 18:
+    raise Exception('No se permiten menores de edad')
+
+except ZeroDivisionError as error:
+  print(error)
+except AssertionError as error:
+  print(error)
+except Exception as error:
+  print(error)
+
+  
+try:
+  print(0/0)
+except Exception as error:
+  print(error)
+  
+print('Hola')
