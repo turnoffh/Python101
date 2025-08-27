570f4376fb7db59cfacdda2429f8461e642f3a4d
David Morales <david4.morales8@gmail.com>
2025-08-27 23:20:49 +0000

    usos del trycatchelsefinally

--- S102/22_errorstry.py
+++ S102/22_errorstry.py
@@ -11,6 +11,8 @@ except AssertionError as error:
   print(error)
 except Exception as error:
   print(error)
+#else: # se ejecuta si no hay error despues de ejecutar lo del try
+#finally: # se ejecuta siempre al final haya o no errores
 
   
 try:
